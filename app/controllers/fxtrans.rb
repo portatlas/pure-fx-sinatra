3a9c3c621ea0ce193fb044d43930b2b8540ea79d
Andrew Vathanakamsang <email": null>
2016-05-19 14:26:35 -0700

    Seperate Buys and Sells in index

--- app/controllers/fxtrans.rb
+++ app/controllers/fxtrans.rb
@@ -4,5 +4,16 @@ get '/fxtrans' do
                    .order('curr_buy ASC')
                    .order('curr_sell ASC')
                    .order('size DESC')
+  @fxbuys = Fxtran.where(trantype: "BUY")
+                   .order('zipcode ASC')
+                   .order('curr_buy ASC')
+                   .order('curr_sell ASC')
+                   .order('size DESC')
+
+  @fxsells = Fxtran.where(trantype: "SELL")
+                   .order('zipcode ASC')
+                   .order('curr_buy ASC')
+                   .order('curr_sell ASC')
+                   .order('size DESC')
   erb :'fxtrans/index'
 end
