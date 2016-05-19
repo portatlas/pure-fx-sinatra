d066b2a69090ea112233e21f401a07bd29ae4d00
Andrew Vathanakamsang <email": null>
2016-05-19 09:06:43 -0700

    Add sinatra skeleton

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+
+  include BCrypt
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  # has_secure_password
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.password == password
+      @user
+    else
+      nil
+    end
+  end
+
+end
