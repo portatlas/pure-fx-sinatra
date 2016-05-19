dc1e4dce11457533c023ec04df0808468e81b559
Andrew Vathanakamsang <email": null>
2016-05-19 12:29:00 -0700

    Sort index page

--- app/controllers/fxtrans.rb
+++ app/controllers/fxtrans.rb
@@ -1,4 +1,8 @@
 get '/fxtrans' do
-  @fxtrans = Fxtran.all #define instance variable for view
-  erb :'fxtrans/index' #show all fxtrans view (index)
+  @fxtrans = Fxtran.all
+                   .order('zipcode ASC')
+                   .order('curr_buy ASC')
+                   .order('curr_sell ASC')
+                   .order('size DESC')
+  erb :'fxtrans/index'
 end
