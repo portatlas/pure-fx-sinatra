425cf9fe6f066df55740b491f170e96c3c27871c
Andrew Vathanakamsang <email": null>
2016-05-19 12:50:59 -0700

    Update seed file for smaller transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,24 @@ require 'faker'
 User.delete_all
 Fxtran.delete_all
 
-users = 20.times.map do
+users = 10.times.map do
   User.create!( :first_name => Faker::Name.first_name,
     :last_name  => Faker::Name.last_name,
     :email      => Faker::Internet.email,
     :password   => 'password' )
 end
 
-20.times do
+5.times do
   random_user = User.all.sample
+  available_currency = ["USD", "EUR"]
+  cur1 = available_currency.sample
+  cur2 = available_currency.reject{|cur| cur == cur1}.sample
   Fxtran.create( requestor_id: random_user.id,
                      trantype: ["BUY", "SELL"].sample,
                          size: rand(10..51),
-                     curr_buy: ["USD", "EUR", "CHF"].sample,
-                    curr_sell: ["AUD", "MXN", "THB"].sample,
+                     curr_buy: cur1,
+                    curr_sell: cur2,
                        status: "PENDING",
-                      zipcode: [14532, 34595, 56706, 78903, 91834].sample )
+                      zipcode: [14532, 34595].sample )
 end
 
