6e78d9f57964d2dde90198be8e6881de03c861d9
Andrew Vathanakamsang <email": null>
2016-05-20 11:33:37 -0700

    Have working mvp but need to refactor

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -7,13 +7,13 @@ post '/login' do
     session[:user_id] = user.id
     redirect "/users/#{user.id}"
   else
-    redirect '/login'
+    @login_error = true
+    erb :'/users/login', locals: { login_error: @login_error }
   end
 end
 
 get '/logout' do
   session.clear
-
   redirect '/'
 end
 
